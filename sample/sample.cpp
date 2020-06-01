4f886e87503be0f0036cec97e45a68ecda7a28e2
Allenhe123 <931317255@qq.com>
2020-06-02 00:03:50 +0800

    update

--- sample/sample.cpp
+++ sample/sample.cpp
@@ -1,10 +1,44 @@
 #include "../mgr.h"
+#include <cstdio>
 
 using namespace df;
 
+
+Task process1(const Task& t) {
+
+    printf("process1 called\n");
+
+    return nullptr;
+}
+
+Task process2(const Task& t) {
+
+    printf("process1 called\n");
+    return nullptr;
+}
+
+Task process3(const Task& t) {
+
+    printf("process1 called\n");
+    return nullptr;
+}
+
 int main(int argc, char* argv[]) {
     GraphMgr::Instance()->CreateGraph("sample.json");
-    // GraphMgr::Instance()->CleanUp();
+    
+
+    EnginePortID id;
+    id.graph_id = 100;
+    id.engine_id = 1000;
+    id.port_id = 0;
+    GraphMgr::Instance()->SetFunctor(id, process1);
+    id.engine_id = 1001;
+    GraphMgr::Instance()->SetFunctor(id, process2);
+    id.engine_id = 1002;
+    GraphMgr::Instance()->SetFunctor(id, process3);
+
+
+    GraphMgr::Instance()->CleanUp();
 
     return 0;
 }
